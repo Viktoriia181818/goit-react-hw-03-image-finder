4a4891f7a4450f1dc519d802abcbd7d044645e3e
Viktoriia181818 <vcibulskaa4@gmail.com>
2023-02-07 15:42:16 +0200

    final hw 3.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,114 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { Api } from './Api/Api';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+import React from 'react';
+
+export class App extends Component {
+  state = {
+    images: [],
+    isLoading: false,
+    currentSearch: '',
+    pageNum: 1,
+    modalOpen: false,
+    modalImg: '',
+    modalAlt: '',
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ isLoading: false });
+    const inputForSearch = e.target.elements.inputForSearch;
+    if (inputForSearch.value.trim() === '') {
+      return toast.error('🦄 Please enter the text!');
+    } else {
+      this.setState({ isLoading: true });
+    }
+    
+    const response = await  Api(inputForSearch.value, 1);
+    this.setState({
+      images: response,
+      isLoading: false,
+      currentSearch: inputForSearch.value,
+      pageNum: 1,
+    });
+  };
+
+  handleClickMore = async () => {
+    const response = await  Api(
+      this.state.currentSearch,
+      this.state.pageNum + 1
+    );
+    this.setState({
+      images: [...this.state.images, ...response],
+      pageNum: this.state.pageNum + 1,
+    });
+  };
+
+  handleImageClick = e => {
+    this.setState({
+      modalOpen: true,
+      modalAlt: e.target.alt,
+      modalImg: e.target.name,
+    });
+  };
+
+  handleModalClose = () => {
+    this.setState({
+      modalOpen: false,
+      modalImg: '',
+      modalAlt: '',
+    });
+  };
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.handleModalClose();
+    }
+  };
+
+  async componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr',
+          gridGap: '16px',
+          paddingBottom: '24px',
+        }}
+      >
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
+          <React.Fragment>
+            <Searchbar onSubmit={this.handleSubmit} />
+            <ImageGallery
+              onImageClick={this.handleImageClick}
+              images={this.state.images}
+            />
+            {this.state.images.length > 0 ? (
+              <Button onClick={this.handleClickMore} />
+            ) : null}
+          </React.Fragment>
+        )}
+        {this.state.modalOpen ? (
+          <Modal
+            src={this.state.modalImg}
+            alt={this.state.modalAlt}
+            handleClose={this.handleModalClose}
+          />
+        ) : null}
+        <ToastContainer />
+      </div>
+    );
+  }
+}
